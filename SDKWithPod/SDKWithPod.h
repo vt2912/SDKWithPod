39d24a3f2c47c2246cf59d43420ac58f60d8a4ee
Vu Thang <thangv@Vus-Mac-mini.local>
2018-01-04 09:07:32 +0700

    Initial Commit

---
+++ SDKWithPod/SDKWithPod.h
@@ -0,0 +1,19 @@
+//
+//  SDKWithPod.h
+//  SDKWithPod
+//
+//  Created by Vu Thang on 1/4/18.
+//  Copyright © 2018 Vu Thang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDKWithPod.
+FOUNDATION_EXPORT double SDKWithPodVersionNumber;
+
+//! Project version string for SDKWithPod.
+FOUNDATION_EXPORT const unsigned char SDKWithPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKWithPod/PublicHeader.h>
+
+
